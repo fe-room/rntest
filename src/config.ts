a6545411733c2b8bc46b3f05854d195997463582
chenmeng <t.chenmeng@sino-commercial.com>
2023-06-21 11:42:56 +0800

    feat: first push

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+const devApiHost = 'https://api.sino-otm.com';
+
+export const devConfig = {
+  host: `${devApiHost}/japi`,
+  mall_host: 'https://apitestv3.mekoomall.com/mallapi_uat',
+  net_host: `${devApiHost}/napi`,
+};
